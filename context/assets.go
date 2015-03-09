3cf0ef3c338538684bdea3e4cc5d7095b22472fa
peace0phmind <peace0phmind@gmail.com>
2015-03-09 23:17:01 +0800

    add license head to go file

--- context/assets.go
+++ context/assets.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2015, tbud. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Don't edit this file, file auto modified by bud.asset.
 package context
 
